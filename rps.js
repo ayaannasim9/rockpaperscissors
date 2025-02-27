fc10d5c1e6dc863e13d2ca676597946724bc9833
ayaannasim9 <ayaannasim9@gmail.com>
2025-02-27 23:20:51 +0000

    Added logic for user selecting paper

--- rps.js
+++ rps.js
@@ -6,6 +6,8 @@ let scissor=document.querySelector("#scissors")
 // acessing scores
 let p_score=document.querySelector("#player")
 let c_score=document.querySelector("#comp")
+player_score=0
+computer_score=0
 
 //accessing message
 let msg=document.querySelector("#msg")
@@ -35,4 +37,19 @@ const generate=()=>{
 const check=(choice) =>{
     // console.log(choice)
     comp_choice=generate()
+    // console.log(comp_choice)
+    if(choice=="paper"){
+        if(comp_choice=="rock"){
+            player_score++
+            p_score.innerText=player_score
+            msg.innerText=`Computer selected rock. You Won!`
+        }else if(comp_choice=="scissor"){
+            computer_score++
+            c_score.innerText=computer_score
+            msg.innerText=`Computer selected scissor. You lost!`
+        }
+        else{
+            msg.innerText=`Computer selected paper. It's a draw!`
+        }
+    }
 }
